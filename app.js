ae8b7d49a8391bc28079167a0e2269b3a41ef9c6
danhorz <danielhornaz@gmail.com>
2024-11-24 01:07:20 -0500

    algoritmos

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const btnEnter=document.getElementById("enter");
+const btnAlgoritmos=document.getElementById("algoritmos");
 btnEnter.addEventListener('click',function(){
 window.location.href="./enterGuide/indexEnterGuide.html";
+});
+btnAlgoritmos.addEventListener('click',function(){
+window.location.href="./algoritmos/algoritmos.html";
 });
